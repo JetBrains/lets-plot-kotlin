b6b93726c9404f6367f461fb367aabf7a1746b59
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2019-06-12 18:44:41 -0400

    sync

--- src/sandbox/Script0.kt
+++ src/sandbox/Script0.kt
@@ -1,5 +1,6 @@
 package sandbox
 
+import com.jetbrains.datalore.plot.dsl.Geom.area
 import com.jetbrains.datalore.plot.dsl.Geom.point
 import com.jetbrains.datalore.plot.dsl.Pos.dodge
 import com.jetbrains.datalore.plot.dsl.Pos.position_dodge
@@ -13,6 +14,8 @@ var df = null
 var p = ggplot(data = df) +
         geom_area({ x = "X" }, size = 3) +
         geom_point(data = df, color = "red") +
+        geom_area({ y = "Y"; fill = "F" }, data = df) +
+        stat_density(geom = area({ y = "Y" }, linetype = 1)) +
         stat_density(geom = point({ y = "Y" }, shape = 1)) +
         geom_area(position = dodge) +
         geom_area(position = position_dodge(width = 10))
