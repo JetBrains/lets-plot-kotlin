b269a2451b2905333a914f3babe4be37dfa51394
Mikhail Koroteev <horis.mkoroteev@gmail.com>
2019-11-25 20:39:18 +0300

    Add scale_x_datetime.

---
+++ plot-api/src/main/kotlin/jetbrains/datalorePlot/scale/DateTime.kt
@@ -0,0 +1,26 @@
+@file:Suppress("FunctionName")
+
+package jetbrains.datalorePlot.scale
+
+import jetbrains.datalorePlot.intern.Options
+import jetbrains.datalorePlot.intern.Scale
+import jetbrains.datalore.plot.base.Aes
+import jetbrains.datalore.plot.config.Option.Scale.DATE_TIME
+
+
+fun scale_x_datetime(
+    name: String? = null,
+    breaks: List<Any>? = null,
+    labels: List<String>? = null,
+    limits: List<Any>? = null,
+    expand: Any? = null,
+    na_value: Any? = null
+) = Scale(
+    Aes.X,
+    name, breaks, labels, limits, expand, na_value, null, null,
+    Options(
+        mapOf(
+            DATE_TIME to true
+        )
+    )
+)
