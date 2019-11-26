fe23bf6158b31256b4ebcfed3a571b33e790a708
Mikhail Koroteev <horis.mkoroteev@gmail.com>
2019-11-26 16:38:51 +0300

    Add x to BinAesthetics.

--- plot-api/src/main/kotlin/jetbrains/datalorePlot/intern/layer/stat/BinAesthetics.kt
+++ plot-api/src/main/kotlin/jetbrains/datalorePlot/intern/layer/stat/BinAesthetics.kt
@@ -4,9 +4,11 @@ import jetbrains.datalorePlot.intern.Options
 import jetbrains.datalorePlot.intern.layer.OptionsCapsule
 
 interface BinAesthetics : OptionsCapsule {
+    val x: Any?
     val weight: Any?
 
     override fun seal() = Options.of(
-        "weight" to weight
+        "weight" to weight,
+        "x" to x
     )
 }
--- plot-api/src/main/kotlin/jetbrains/datalorePlot/intern/layer/stat/BinMapping.kt
+++ plot-api/src/main/kotlin/jetbrains/datalorePlot/intern/layer/stat/BinMapping.kt
@@ -1,6 +1,7 @@
 package jetbrains.datalorePlot.intern.layer.stat
 
 class BinMapping(
+    override var x: Any? = null,
     override var weight: Any? = null
 ) : BinAesthetics {
     override fun seal() = super.seal()
