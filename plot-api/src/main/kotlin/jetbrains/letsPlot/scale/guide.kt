8b576fa2f74026c2e53321791fdf197142f7c01e
IKupriyanov-HORIS <ikupriyanov@horis.ru>
2019-11-27 20:01:13 +0300

    Add guide_colorbar(), but looks like it doesn't work

--- plot-api/src/main/kotlin/jetbrains/letsPlot/scale/guide.kt
+++ plot-api/src/main/kotlin/jetbrains/letsPlot/scale/guide.kt
@@ -6,22 +6,40 @@ import jetbrains.letsPlot.intern.filterNonNullValues
  * Legend guide.
  * Legend type guide shows key (i.e., geoms) mapped onto values.
  *
- * @param nrow int, optional
- * Number of rows in legend's guide
- * @param ncol int, optional
- * Number of columns in legend's guide
- * @param byrow boolean, optional
- * Type of output: by row (default), or by column
+ * @param nrow A number of rows in legend's guide
+ * @param ncol A number of columns in legend's guide
+ * @param byrow A type of output: by row (default), or by column
  */
 @Suppress("FunctionName")
 fun guide_legend(
     nrow: Int? = null,
     ncol: Int? = null,
     byrow: Boolean? = null
-) : Map<String, Any> {
+): Map<String, Any> {
     return mapOf(
         "nrow" to nrow,
         "ncol" to ncol,
         "byrow" to byrow
+    ).filterNonNullValues()
+}
+
+/**
+ * Continuous color bar guide.
+ * Color bar guide shows continuous color scales mapped onto values.
+ * Color bar is available with scale_fill and scale_color.
+ * @param barwidth Color bar width
+ * @param barheight Color bar height
+ * @param nbin Number of bins in color bar
+ */
+@Suppress("FunctionName")
+fun guide_colorbar(
+    barwidth: Double? = null,
+    barheight: Double? = null,
+    nbin: Int? = null
+): Map<String, Any> {
+    return mapOf(
+        "barwidth" to barwidth,
+        "barheight" to barheight,
+        "nbin" to nbin
     ).filterNonNullValues()
 }
