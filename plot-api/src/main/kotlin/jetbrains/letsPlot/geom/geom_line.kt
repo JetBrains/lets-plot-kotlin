6c5222420a88a64468fbec485855379379304d88
Mikhail Koroteev <horis.mkoroteev@gmail.com>
2019-11-30 00:37:30 +0300

    Add dokka docs.

--- plot-api/src/main/kotlin/jetbrains/letsPlot/geom/geom_line.kt
+++ plot-api/src/main/kotlin/jetbrains/letsPlot/geom/geom_line.kt
@@ -10,6 +10,33 @@ import jetbrains.letsPlot.intern.layer.geom.LineAesthetics
 import jetbrains.letsPlot.intern.layer.geom.LineMapping
 
 @Suppress("ClassName")
+/**
+ * Connect points in order of the variable on the x-axis
+ * @param data dictionary or pandas DataFrame, optional.
+ *     The data to be displayed in this layer. If None, the default, the data
+ *     is inherited from the plot data as specified in the call to [ggplot][jetbrains.datalorePlot.ggplot].
+ * @param stat string, optional.
+ *     The statistical transformation to use on the data for this layer, as a string. Supported transformations:
+ *     "identity" (leaves the data unchanged), "count" (counts number of points with same x-axis coordinate),
+ *     "bin" (counts number of points with x-axis coordinate in the same bin), "smooth" (performs smoothing -
+ *     linear default)
+ * @param position string, optional.
+ *     Position adjustment, either as a string ("identity", "stack", "dodge", ...), or the result of a call to a
+ *     position adjustment function.
+ * @param x x-axis value.
+ * @param y y-axis value.
+ * @param alpha transparency level of a point
+ *     Understands numbers between 0 and 1.
+ * @param color (colour) color of a geometry.
+ *     Can be continuous or discrete. For continuous value this will be a color gradient between two colors.
+ * @param linetype type of the line.
+ *     Codes and names: 0 = "blank", 1 = "solid", 2 = "dashed", 3 = "dotted", 4 = "dotdash",
+ *     5 = "longdash", 6 = "twodash".
+ * @param size line width.
+ * @param mapping set of aesthetic mappings.
+ *     Aesthetic mappings describe the way that variables in the data are
+ *     mapped to plot "aesthetics".
+ */
 class geom_line(
     data: Any? = null,
     stat: StatOptions = Stat.identity,
