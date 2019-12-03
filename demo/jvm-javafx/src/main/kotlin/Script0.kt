e96ea4f08f1b538fda5d66df2bca79b94cff67e6
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2019-12-03 15:20:04 -0500

    Add copyright profile and update copyright notice in project files.

--- demo/jvm-javafx/src/main/kotlin/Script0.kt
+++ demo/jvm-javafx/src/main/kotlin/Script0.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
 import jetbrains.letsPlot.Geom.area
 import jetbrains.letsPlot.Geom.point
 import jetbrains.letsPlot.Pos.dodge
--- plot-api/src/main/kotlin/jetbrains/letsPlot/scale/Manual.kt
+++ plot-api/src/main/kotlin/jetbrains/letsPlot/scale/Manual.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
 package jetbrains.letsPlot.scale
 
 //def scale_fill_manual(values, name=None, breaks=None, labels=None, limits=None, expand=None, na_value=None, guide=None):
