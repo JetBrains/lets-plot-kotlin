771de6d58999887c035ccccdf162b0503eb33f58
OLarionova-HORIS <olarionova@horis.ru>
2020-06-18 19:15:55 +0300

    Add geom_smooth.

---
+++ demo/jvm-javafx/src/main/kotlin/frontendContextDemo/scripts/Smooth.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package frontendContextDemo.scripts
+
+import frontendContextDemo.ScriptInJfxContext
+import jetbrains.letsPlot.geom.geom_point
+
+import jetbrains.letsPlot.geom.geom_smooth
+import jetbrains.letsPlot.lets_plot
+import jetbrains.letsPlot.theme
+
+object Smooth {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        ScriptInJfxContext.eval("Smooth") {
+            val n = 150
+            val rand = java.util.Random()
+            val data = mapOf<String, Any>(
+                "x" to List(n) { rand.nextGaussian() },
+                "y" to List(n) { rand.nextGaussian() }
+            )
+            val p = lets_plot(data) { x = "x"; y = "y" } +
+                    theme().legendPosition_none() +
+                    geom_point(shape=21, fill="yellow", color="blue") +
+                    geom_smooth(method="lm", deg=5, size=1.2)
+
+            p.show()
+        }
+    }
+}
