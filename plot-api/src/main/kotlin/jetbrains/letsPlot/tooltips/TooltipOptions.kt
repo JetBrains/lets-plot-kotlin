c1808ad8331341c3c1306f7b69a14ac6bd54fdcc
Olga Larionova <46743085+OLarionova-HORIS@users.noreply.github.com>
2021-03-22 19:56:42 +0300

    Add parameter descriptions (#60)
    * Add parameter descriptions for 'tooltips' functions.
    
    * Add parameter descriptions for the 'guide' function.

--- plot-api/src/main/kotlin/jetbrains/letsPlot/tooltips/TooltipOptions.kt
+++ plot-api/src/main/kotlin/jetbrains/letsPlot/tooltips/TooltipOptions.kt
@@ -48,6 +48,20 @@ class TooltipOptions() {
      * Defines the format for displaying the value.
      * The format will be applied to the mapped value in the default tooltip
      * or to the corresponding value specified in the line template.
+     *
+     * @param field Aesthetic or variable name to apply the format to.
+     *     The field name starts with a '^' prefix for aesthetics, variable names are specified without prefix or with a '@' prefix.
+     *     It's possible to set the format for all positional aesthetics:
+     *        field = "^X" - for all positional x;
+     *        field = "^Y" - for all positional y.
+     * @param format A number format ('1.f') or a string template ('{.1f}').
+     *     The numeric format for non-numeric value will be ignored.
+     *     If you need to include a brace character in the literal text, it can be escaped by doubling: {{ and }}, e.g.,
+     *       .format('^color', '{{ {.1f} }}') -> "{ 17.0 }"
+     *       .format('model', '{} {{text}}') -> "mustang {text}"
+     *     The string template in format will allow to change lines for the default tooltip without 'line' specifying.
+     *     Also the template will change the line for outliers.
+     *     Aes and var formats are not interchangeable, i.e. var format will not be applied to aes, mapped to this variable.
      */
     fun format(field: String, format: String): TooltipOptions {
         return addListOption(
@@ -60,6 +74,30 @@ class TooltipOptions() {
 
     /**
      * Specifies the string template to use in the multi-line tooltip.
+     *
+     * @param template A line template to add the tooltip with a label.
+     *     Variables and aesthetics can be accessed via a special syntax:
+     *        - ^color for aes
+     *        - @year for variable
+     *        - @{number of cylinders} for variable with spaces in the name
+     *        - @{square m^2} for variable with spaces and '^' symbol in the name
+     *        - @nameWith^ for the variable with '^' symbol in its name
+     *     A '^' symbol can be escaped with a backslash, a brace character in the literal text - by doubling:
+     *        .line("text") -> "text"
+     *        .line("{{text}}") -> "{text}"
+     *        .line("@model") -> "mustang"
+     *        .line("{{@model}}") -> "{mustang}"
+     *     The specified 'line' for outlier will move it to the general multi-line tooltip.
+     *     The default tooltip has a label before the value, usually containing the name of the mapped variable.
+     *     It has it's own behaviour, like blank label for axis aesthetics.
+     *     This default label can be set in template using a pair of symbols '@|'.
+     *     The label can be overridden by specifying a string value before '|' symbol.
+     *     Within the tooltip line the label is left-aligned, the formed by template string is right-aligned.
+     *     If a label is not specified, the string will be centered in the tooltip. For example:
+     *        - line("^color"): no label, value is centered;
+     *        - line("|^color"): label is empty, value is right-aligned;
+     *        - line("@|^color"): default label is used, value is right-aligned;
+     *        - line("my label|^color"): label is specified, value is right-aligned.
      */
     fun line(template: String): TooltipOptions {
         return addListOption(TOOLTIP_LINES, template)
@@ -67,6 +105,11 @@ class TooltipOptions() {
 
     /**
      * Specifies a fixed position for the general tooltip.
+     *
+     * @param position Position of the plot to move the general tooltip:
+     *         ["top_left" | "top_center" | "top_right" |
+     *         "middle_left" | "middle_center" | "middle_right" |
+     *         "bottom_left" | "bottom_center" | "bottom_right"]
      */
     fun anchor(position: String): TooltipOptions {
         return setOption(TOOLTIP_ANCHOR, position)
@@ -74,6 +117,8 @@ class TooltipOptions() {
 
     /**
      * Specifies a minimum width of a general tooltip in pixels.
+
+     * @param value Minimum width of the general tooltip.
      */
     fun minWidth(value: Number): TooltipOptions {
         return setOption(TOOLTIP_MIN_WIDTH, value)
@@ -81,6 +126,8 @@ class TooltipOptions() {
 
     /**
      * Specifies a color of a general tooltip.
+     *
+     * @param value The color for the general tooltip.
      */
     fun color(value: String): TooltipOptions {
         return setOption(TOOLTIP_COLOR, value)
