f2d7188152df33a2845f11bbb1d81ec1095a0999
Artem Smirnov <asmirnov@horis.ru>
2021-08-27 17:33:52 +0300

    Generate the api-reference with Dokka. (#89)

---
+++ docs/api-reference/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":plot-api:dokkaHtml/commonMain":[],":plot-api:dokkaHtml/jsMain":[":plot-api:dokkaHtml/commonMain"],":plot-api:dokkaHtml/jvmMain":[":plot-api:dokkaHtml/commonMain"]}'
