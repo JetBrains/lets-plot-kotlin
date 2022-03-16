adde5626f5079ffacac97e18b3dce23bc6edfd29
Olga Larionova <olarionova@horis.ru>
2022-03-16 13:34:05 +0300

    Add 'plotSubtitle' and 'plotCaption' parameters in theme().

--- demo/jvm-javafx/src/main/kotlin/frontendContextDemo/scripts/Theme.kt
+++ demo/jvm-javafx/src/main/kotlin/frontendContextDemo/scripts/Theme.kt
@@ -6,27 +6,49 @@
 package frontendContextDemo.scripts
 
 import frontendContextDemo.ScriptInJfxContext
+import jetbrains.datalore.base.values.Color
 import jetbrains.letsPlot.elementBlank
+import jetbrains.letsPlot.elementText
 import jetbrains.letsPlot.geom.geomTile
 import jetbrains.letsPlot.ggplot
+import jetbrains.letsPlot.label.labs
 import jetbrains.letsPlot.theme
+import jetbrains.letsPlot.themeGrey
 
 object Theme {
     @JvmStatic
     fun main(args: Array<String>) {
-        ScriptInJfxContext.eval("Theme") {
+        ScriptInJfxContext.eval("Theme", maxCol = 2) {
             val xs = (-64..64).toList()
             val data = mapOf("x" to xs)
 
             val p = ggplot(data) +
-                    geomTile(width = 1.0, height = 10.0) { x = "x"; color = "x"; fill = "x" } +
-                    theme(
-                        axisTextY = "blank",
-                        axisTicksY = elementBlank(),
-                        axisTitleY = "blank"
-                    ).legendPositionNone()
+                    geomTile(width = 1.0, height = 10.0) { x = "x"; color = "x"; fill = "x" }
 
-            p.show()
+            (p + theme().legendPositionNone())
+                .show()
+
+            // compose themes --> the same
+            (p + themeGrey() + theme().legendPositionNone())
+                .show()
+
+            (p + theme(
+                axisTextY = "blank",
+                axisTicksY = elementBlank(),
+                axisTitleY = "blank"
+            ).legendPositionNone()).show()
+
+            // Add plot labels and use colors
+            (p + labs(
+                title = "The plot title",
+                subtitle = "The plot subtitle",
+                caption = "The plot caption"
+            ) + theme(
+                plotTitle = elementText(Color.DARK_BLUE),
+                plotSubtitle = elementText(Color.GRAY),
+                plotCaption = elementText(Color.DARK_GREEN)
+            ).legendPositionNone())
+                .show()
         }
     }
 }
