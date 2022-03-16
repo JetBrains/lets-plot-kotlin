c2468d8abed7cb1faf9a38065a6da19f714b4ecb
Olga Larionova <olarionova@horis.ru>
2022-03-16 13:27:38 +0300

    Add 'subtitle' parameter to ggtitle(). Add 'subtitle' and 'caption' parameters to labs(). Update examples.

--- demo/jvm-javafx/src/main/kotlin/frontendContextDemo/scripts/Title.kt
+++ demo/jvm-javafx/src/main/kotlin/frontendContextDemo/scripts/Title.kt
@@ -22,12 +22,25 @@ object Title {
                 "y" to (0..100).map { sin(it * 2 * PI / 100) }
             )
 
-            val p = letsPlot(dat) +
-                    geomPoint { x = "x"; y = "y" } +
-                    ggtitle("Sine function") +
-                    scaleSizeArea(maxSize = 70, guide = "none")
+            run {
+                val p = letsPlot(dat) +
+                        geomPoint { x = "x"; y = "y" } +
+                        ggtitle("Sine function", subtitle = "Using the ggtitle() function") +
+                        scaleSizeArea(maxSize = 70, guide = "none")
 
-            p.show()
+                p.show()
+            }
+
+            run {
+                // Use multiple lines
+                val p = letsPlot(dat) +
+                        geomPoint { x = "x"; y = "y" } +
+                        ggtitle("The plot title:\nSine function",
+                            subtitle = "The plot subtitle:\nUsing the ggtitle() function") +
+                        scaleSizeArea(maxSize = 70, guide = "none")
+
+                p.show()
+            }
         }
     }
 }
