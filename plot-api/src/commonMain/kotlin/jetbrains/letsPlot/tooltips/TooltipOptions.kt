18c61f0f4be312750061968385ce8bf860bcd940
Olga Larionova <olarionova@horis.ru>
2022-03-16 12:50:38 +0300

    Tooltip title() function.

--- plot-api/src/commonMain/kotlin/jetbrains/letsPlot/tooltips/TooltipOptions.kt
+++ plot-api/src/commonMain/kotlin/jetbrains/letsPlot/tooltips/TooltipOptions.kt
@@ -119,6 +119,19 @@ class TooltipOptions(variables: List<String>?) {
     }
 
     /**
+     * Specifies the string template to use as a title in the multi-line tooltip.
+     *
+     * @param template A line template to add the tooltip title.
+     *     The specification rules are the same as for the `lines()` function:
+     *     variables and aesthetics can be used in the template.
+     *     The resulting string will be at the beginning of the general tooltip, centered and highlighted in bold.
+     *     A long title can be split into multiple lines using `\n` as a text separator.
+     */
+    fun title(template: String): TooltipOptions {
+        return setOption(TOOLTIP_TITLE, template)
+    }
+
+    /**
      * Specifies a fixed position for the general tooltip.
      *
      * @param position Position of the plot to move the general tooltip:
@@ -166,6 +179,7 @@ class TooltipOptions(variables: List<String>?) {
         private const val FORMAT = Option.TooltipFormat.FORMAT
 
         private const val TOOLTIP_LINES = Option.Layer.TOOLTIP_LINES
+        private const val TOOLTIP_TITLE = Option.Layer.TOOLTIP_TITLE
         private const val TOOLTIP_MIN_WIDTH = Option.Layer.TOOLTIP_MIN_WIDTH
 //        private const val TOOLTIP_COLOR = Option.Layer.TOOLTIP_COLOR
 
