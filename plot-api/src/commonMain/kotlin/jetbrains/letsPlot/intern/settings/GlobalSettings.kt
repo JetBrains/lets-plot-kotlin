8896ee4e835c319bfd6eae99532cf6582e36d03f
Ivan Kupriyanov <ikupriyanov@horis.ru>
2022-04-21 20:19:32 +0300

    Rename

--- plot-api/src/commonMain/kotlin/jetbrains/letsPlot/intern/settings/GlobalSettings.kt
+++ plot-api/src/commonMain/kotlin/jetbrains/letsPlot/intern/settings/GlobalSettings.kt
@@ -13,13 +13,13 @@ import jetbrains.letsPlot.intern.filterNonNullValues
 
 // Environment variables
 const val ENV_HTML_ISOLATED_FRAME = "LETS_PLOT_HTML_ISOLATED_FRAME"
-const val ENV_LETS_PLOT_PLOT_THEME = "LETS_PLOT_PLOT_THEME"
+const val ENV_PLOT_THEME = "LETS_PLOT_PLOT_THEME"
 
 
 internal object GlobalSettings {
     val isolatedFrameContext: Boolean get() = Env.getBool(ENV_HTML_ISOLATED_FRAME) ?: false
 
-    var theme: OptionsMap? = Env.get(ENV_LETS_PLOT_PLOT_THEME)?.let { OptionsMap(THEME, parseJson(it).filterNonNullValues()) }
+    var theme: OptionsMap? = Env.get(ENV_PLOT_THEME)?.let { OptionsMap(THEME, parseJson(it).filterNonNullValues()) }
         set(value) {
             require(value == null || value.kind == THEME) {
                 "Wrong options type. Expected `$THEME` but was `${value!!.kind}`"
