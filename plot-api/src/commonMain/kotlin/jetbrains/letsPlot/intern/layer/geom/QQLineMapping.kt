bf3533129472be211818eec675b9adf33d7732f9
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2022-06-17 14:49:52 -0400

    Move 'sample' to the 1st position in QQMapping, QQLineMapping.

--- plot-api/src/commonMain/kotlin/jetbrains/letsPlot/intern/layer/geom/QQLineMapping.kt
+++ plot-api/src/commonMain/kotlin/jetbrains/letsPlot/intern/layer/geom/QQLineMapping.kt
@@ -9,12 +9,12 @@ import jetbrains.letsPlot.intern.layer.WithGroupOption
 import jetbrains.letsPlot.intern.layer.stat.QQStatAesthetics
 
 class QQLineMapping(
+    override var sample: Any? = null,
     override var alpha: Any? = null,
     override var color: Any? = null,
     override var linetype: Any? = null,
     override var size: Any? = null,
     override var group: Any? = null,
-    override var sample: Any? = null
 ) : QQLineAesthetics, QQStatAesthetics, WithGroupOption {
     override fun seal() = super<QQLineAesthetics>.seal() +
             super<QQStatAesthetics>.seal() + groupOption()
--- plot-api/src/commonMain/kotlin/jetbrains/letsPlot/intern/layer/geom/QQMapping.kt
+++ plot-api/src/commonMain/kotlin/jetbrains/letsPlot/intern/layer/geom/QQMapping.kt
@@ -9,13 +9,13 @@ import jetbrains.letsPlot.intern.layer.WithGroupOption
 import jetbrains.letsPlot.intern.layer.stat.QQStatAesthetics
 
 class QQMapping(
+    override var sample: Any? = null,
     override var alpha: Any? = null,
     override var color: Any? = null,
     override var fill: Any? = null,
     override var shape: Any? = null,
     override var size: Any? = null,
     override var group: Any? = null,
-    override var sample: Any? = null
 ) : QQAesthetics, QQStatAesthetics, WithGroupOption {
     override fun seal() = super<QQAesthetics>.seal() +
             super<QQStatAesthetics>.seal() + groupOption()
