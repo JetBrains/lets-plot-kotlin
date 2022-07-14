72e1c44fe6e6ebf160ba3ec27688f03c3c409c93
Artem Smirnov <asmirnov@horis.ru>
2022-07-14 19:42:19 +0300

    Add target="_blank" to links to jupyter notebooks in the documentation.

--- docs/source/custom.js
+++ docs/source/custom.js
@@ -1,21 +1,25 @@
-const indexHTML = `
-<div class="UnderCoverText custom-under-cover">
-    <div>
-        See also the <a href="https://github.com/JetBrains/lets-plot-kotlin">GitHub repository</a> of the library.
-    </div>
-</div>
-`;
-
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', onDocumentReady, false);
 } else
     onDocumentReady();
 
-
 function onDocumentReady() {
+    addUnderCoverTextToIndex();
+    addTargetBlankToLinks();
+}
+
+function addUnderCoverTextToIndex() {
+    const underCoverText = `
+    <div class="UnderCoverText custom-under-cover">
+        <div>
+            See also the <a href="https://github.com/JetBrains/lets-plot-kotlin" target="_blank">GitHub repository</a> of the library.
+        </div>
+    </div>
+    `;
+
     const coverElem = document.querySelector("#main div.cover");
     if (coverElem.textContent.trim().split(" ").indexOf("Lets-Plot-Kotlin") == 0) {
-        const newElem = htmlToElement(indexHTML);
+        const newElem = htmlToElement(underCoverText);
         coverElem.parentElement.insertBefore(newElem, coverElem.nextSibling);
     }
 }
@@ -24,4 +28,10 @@ function htmlToElement(html) {
     const template = document.createElement('template');
     template.innerHTML = html.trim();
     return template.content.firstChild;
+}
+
+function addTargetBlankToLinks() {
+    document.querySelectorAll('[href*="nbviewer.jupyter.org"]').forEach(function (linkElem) {
+        linkElem.setAttribute("target", "_blank");
+    });
 }
