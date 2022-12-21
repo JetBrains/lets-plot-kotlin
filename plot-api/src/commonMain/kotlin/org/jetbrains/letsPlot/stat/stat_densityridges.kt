cf1cef913f5c8c0443af478009bba383808dec5c
Olga Larionova <olarionova@horis.ru>
2022-12-21 19:11:51 +0300

    Add `statDensityRidges()`.

---
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/stat/stat_densityridges.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.letsPlot.stat
+
+import org.jetbrains.letsPlot.Stat
+import org.jetbrains.letsPlot.intern.GeomKind
+import org.jetbrains.letsPlot.intern.Options
+import org.jetbrains.letsPlot.intern.layer.GeomOptions
+import org.jetbrains.letsPlot.intern.layer.LayerBase
+import org.jetbrains.letsPlot.intern.layer.PosOptions
+import org.jetbrains.letsPlot.intern.layer.SamplingOptions
+import org.jetbrains.letsPlot.intern.layer.geom.AreaRidgesAesthetics
+import org.jetbrains.letsPlot.intern.layer.geom.AreaRidgesMapping
+import org.jetbrains.letsPlot.intern.layer.stat.DensityRidgesStatAesthetics
+import org.jetbrains.letsPlot.intern.layer.stat.DensityRidgesStatParameters
+import org.jetbrains.letsPlot.pos.positionIdentity
+
+@Suppress("ClassName", "SpellCheckingInspection")
+class statDensityRidges(
+    data: Map<*, *>? = null,
+    geom: GeomOptions = GeomOptions(GeomKind.AREA_RIDGES),
+    position: PosOptions = positionIdentity,
+    showLegend: Boolean = true,
+    sampling: SamplingOptions? = null,
+    override val x: Number? = null,
+    override val y: Number? = null,
+    override val height: Number? = null,
+    override val quantile: Number? = null,
+    override val alpha: Number? = null,
+    override val color: Any? = null,
+    override val fill: Any? = null,
+    override val linetype: Any? = null,
+    override val size: Number? = null,
+    override val weight: Number? = null,
+    override val tailsCutoff: Number? = null,
+    override val quantiles: List<Number>? = null,
+    override val bw: Any? = null,
+    override val kernel: String? = null,
+    override val n: Int? = null,
+    override val trim: Boolean? = null,
+    override val adjust: Number? = null,
+    override val fullScanMax: Int? = null,
+    mapping: AreaRidgesMapping.() -> Unit = {}
+) : AreaRidgesAesthetics,
+    DensityRidgesStatAesthetics,
+    DensityRidgesStatParameters,
+    LayerBase(
+        mapping = AreaRidgesMapping().apply(mapping).seal(),
+        data = data,
+        geom = geom,
+        stat = Stat.densityRidges(),
+        position = position,
+        showLegend = showLegend,
+        sampling = sampling
+    ) {
+    override fun seal(): Options {
+        return super<AreaRidgesAesthetics>.seal() +
+                super<DensityRidgesStatAesthetics>.seal() +
+                super<DensityRidgesStatParameters>.seal()
+    }
+}
