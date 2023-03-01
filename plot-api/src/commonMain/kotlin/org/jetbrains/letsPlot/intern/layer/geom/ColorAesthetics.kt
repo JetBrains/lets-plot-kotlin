cba8c1b0afb1b6fc2596d0f8144a7affb86935b9
Olga Larionova <olarionova@horis.ru>
2023-03-01 19:27:56 +0300

    Add `color_by` for contours. Add example.

---
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/ColorAesthetics.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.letsPlot.intern.layer.geom
+
+import org.jetbrains.letsPlot.intern.Options
+import org.jetbrains.letsPlot.intern.OptionsCapsule
+
+interface ColorAesthetics : OptionsCapsule {
+    val color: Any?
+    val fill: Any?
+    val paint_a: Any?
+    val paint_b: Any?
+    val paint_c: Any?
+
+    override fun seal() = Options.of(
+        "color" to color,
+        "fill" to fill,
+        "paint_a" to paint_a,
+        "paint_b" to paint_b,
+        "paint_c" to paint_c
+    )
+}
--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/ContourMapping.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/ContourMapping.kt
@@ -13,14 +13,19 @@ class ContourMapping(
     override var y: Any? = null,
     override var z: Any? = null,
     override var alpha: Any? = null,
-    override var color: Any? = null,
     override var linetype: Any? = null,
     override var size: Any? = null,
     override var speed: Any? = null,
     override var flow: Any? = null,
-    override var group: Any? = null
-) : PathAesthetics, ContourStatAesthetics, WithGroupOption {
+    override var group: Any? = null,
+    override var fill: Any? = null,
+    override var color: Any? = null,
+    override var paint_a: Any? = null,
+    override var paint_b: Any? = null,
+    override var paint_c: Any? = null
+) : PathAesthetics, ContourStatAesthetics, WithGroupOption, ColorAesthetics {
     override fun seal() = super<PathAesthetics>.seal() +
             super<ContourStatAesthetics>.seal() +
-            groupOption()
+            groupOption() +
+            super<ColorAesthetics>.seal()
 }
