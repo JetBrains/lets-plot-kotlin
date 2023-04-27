114cdac95ba93d6f1c7117f576534a71cc81b98c
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2023-04-27 18:03:05 -0400

    Add Batik "Imshow" demo (not working).

---
+++ demo/jvm-batik/src/main/kotlin/frontendContextDemo/scripts/Imshow.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023. JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package frontendContextDemo.scripts
+
+import frontendContextDemo.ScriptInBatikContext
+import org.jetbrains.letsPlot.geom.RasterData
+import org.jetbrains.letsPlot.geom.geomImshow
+import org.jetbrains.letsPlot.letsPlot
+
+object Imshow {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        ScriptInBatikContext.eval("Imshow") {
+
+            // Cases identical to onec in org.jetbrains.letsPlot.ImshowTest
+            val imdataList = listOf(
+                // simple test
+                RasterData.create(
+                    listOf(
+                        listOf(listOf(150, 0, 0), listOf(0, 150, 0)),
+                        listOf(listOf(0, 0, 150), listOf(150, 150, 0)),
+                    )
+                ),
+
+                // gray 2 x 3 matrix of byte
+                RasterData.create(
+                    listOf(
+                        listOf(0.toByte(), 0.toByte(), 0.toByte()),
+                        listOf(255.toByte(), 255.toByte(), 255.toByte())
+                    )
+                )
+            )
+
+            imdataList.forEach {
+                (letsPlot() + geomImshow(it)).show()
+            }
+        }
+    }
+}
