afa89897c5a7763a4e4fc436cc707373e08f3dc9
Olga Larionova <olarionova@horis.ru>
2023-05-03 18:37:37 +0300

    Add parameter descriptions.

--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/geom/geom_lollipop.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/geom/geom_lollipop.kt
@@ -21,6 +21,66 @@ import org.jetbrains.letsPlot.pos.positionIdentity
 import org.jetbrains.letsPlot.tooltips.TooltipOptions
 
 @Suppress("ClassName", "SpellCheckingInspection")
+/**
+ * Draws lollipop chart.
+ *
+ * ## Examples
+ *
+ *
+ * @param data The data to be displayed in this layer. If null, the default, the data
+ *  is inherited from the plot data as specified in the call to [letsPlot][org.jetbrains.letsPlot.letsPlot].
+ * @param stat The statistical transformation to use on the data for this layer.
+ *  Supported transformations: `Stat.identity`, `Stat.bin()`, `Stat.count()`, etc. see [Stat][org.jetbrains.letsPlot.Stat].
+ * @param position Position adjustment: `positionIdentity`, `positionStack()`, `positionDodge()`, etc. see
+ *  [Position](https://lets-plot.org/kotlin/-lets--plot--kotlin/org.jetbrains.letsPlot.pos/).
+ * @param showLegend default = true.
+ *  false - do not show legend for this layer.
+ * @param sampling Result of the call to the `samplingXxx()` function.
+ *  To prevent any sampling for this layer pass value `samplingNone`.
+ *  For more info see [sampling.md](https://github.com/JetBrains/lets-plot-kotlin/blob/master/docs/sampling.md).
+ * @param tooltips Result of the call to the `layerTooltips()` function.
+ *  Specifies appearance, style and content.
+ * @param orientation Specifies the axis that the baseline should run along, default = "x".
+ *  Possible values: "x", "y".
+ * @param x X-axis value.
+ * @param y Y-axis value.
+ * @param size Size of the point.
+ * @param stroke Width of the shape border. Applied only to the shapes having border.
+ * @param linewidth Stick width.
+ * @param color Color of the geometry.
+ *  String in the following formats:
+ *  - RGB/RGBS (e.g. "rgb(0, 0, 255)")
+ *  - HEX (e.g. "#0000FF")
+ *  - color name (e.g. "red")
+ *
+ *  Or an instance of the `java.awt.Color` class.
+ * @param fill Color to paint shape's inner points.
+ *  Is applied only to the points of shapes having inner points.
+ *  String in the following formats:
+ *  - RGB/RGBS (e.g. "rgb(0, 0, 255)")
+ *  - HEX (e.g. "#0000FF")
+ *  - color name (e.g. "red")
+ *
+ *  Or an instance of the `java.awt.Color` class.
+ * @param alpha Transparency level of a layer. Understands numbers between 0 and 1.
+ * @param shape Shape of the point.
+ * @param linetype Type of the stick line.
+ *  Codes and names: 0 = "blank", 1 = "solid", 2 = "dashed", 3 = "dotted", 4 = "dotdash",
+ *  5 = "longdash", 6 = "twodash".
+ * @param fatten default = 2.5.
+ *  A multiplicative factor applied to size of the point.
+ * @param slope The baseline slope.
+ * @param intercept The value of y at the point where the baseline crosses the y-axis.
+ * @param dir Direction of the lollipop stick, default = "v".
+ *  Possible values: "v" for vertical, "h" for horizontal, "s" for orthogonal to the baseline.
+ * @param colorBy default = "color" ("fill", "color", "paint_a", "paint_b", "paint_c").
+ *  Defines the color aesthetic for the geometry.
+ * @param fillBy default = "fill" ("fill", "color", "paint_a", "paint_b", "paint_c").
+ *  Defines the fill aesthetic for the geometry.
+ * @param mapping Set of aesthetic mappings.
+ *  Aesthetic mappings describe the way that variables in the data are
+ *  mapped to plot "aesthetics".
+ */
 class geomLollipop(
     data: Map<*, *>? = null,
     stat: StatOptions = Stat.identity,
