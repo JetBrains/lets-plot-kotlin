b8e6bd84d63088c3877ec1dd77b83dc73c9c875b
Artem Smirnov <asmirnov@horis.ru>
2023-08-29 13:59:39 +0200

    KDocs for the statSmooth().

--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/stat/stat_smooth.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/stat/stat_smooth.kt
@@ -20,6 +20,66 @@ import org.jetbrains.letsPlot.intern.layer.stat.SmoothStatParameters
 import org.jetbrains.letsPlot.pos.positionIdentity
 
 @Suppress("ClassName")
+/**
+ * Displays a smoothed conditional mean.
+ *
+ * @param data The data to be displayed in this layer. If null, the default, the data
+ *  is inherited from the plot data as specified in the call to [letsPlot][org.jetbrains.letsPlot.letsPlot].
+ * @param geom The geometry to display the ridges stat for this layer, default is smoothed line,
+ *  see [Geom][org.jetbrains.letsPlot.Geom].
+ * @param position Position adjustment: `positionIdentity`, `positionStack()`, `positionDodge()`, etc. see
+ *  [Position](https://lets-plot.org/kotlin/-lets--plot--kotlin/org.jetbrains.letsPlot.pos/).
+ * @param showLegend default = true.
+ *  false - do not show legend for this layer.
+ * @param sampling Result of the call to the `samplingXxx()` function.
+ *  To prevent any sampling for this layer pass value `samplingNone`.
+ *  For more info see [sampling.md](https://github.com/JetBrains/lets-plot-kotlin/blob/master/docs/sampling.md).
+ * @param x X-axis value.
+ * @param y Predicted (smoothed) value.
+ * @param ymin Lower pointwise confidence interval around the mean.
+ * @param ymax Upper pointwise confidence interval around the mean.
+ * @param size Lines width.
+ *  Defines line width for conditional mean and confidence bounds lines.
+ * @param linetype Type of the line.
+ *  Codes and names: 0 = "blank", 1 = "solid", 2 = "dashed", 3 = "dotted", 4 = "dotdash",
+ *  5 = "longdash", 6 = "twodash".
+ * @param color Color of the geometry.
+ *  String in the following formats:
+ *  - RGB/RGBA (e.g. "rgb(0, 0, 255)")
+ *  - HEX (e.g. "#0000FF")
+ *  - color name (e.g. "red")
+ *  - role name ("pen", "paper" or "brush")
+ *
+ *  Or an instance of the `java.awt.Color` class.
+ * @param fill Fill color.
+ *  String in the following formats:
+ *  - RGB/RGBA (e.g. "rgb(0, 0, 255)")
+ *  - HEX (e.g. "#0000FF")
+ *  - color name (e.g. "red")
+ *  - role name ("pen", "paper" or "brush")
+ *
+ *  Or an instance of the `java.awt.Color` class.
+ * @param alpha Transparency level of a layer. Understands numbers between 0 and 1.
+ * @param method default = "lm".
+ *  Smoothing method: lm (Linear Model) or loess (Locally Estimated Scatterplot Smoothing).
+ * @param n default = 80. Number of points to evaluate smoother at.
+ * @param level default = 0.95. Level of confidence interval to use.
+ * @param se default = true. To display confidence interval around smooth.
+ * @param span default = 0.5.
+ *  Only for LOESS method. The fraction of source points closest to the current point
+ *  is taken into account for computing a least-squares regression. A sensible value is usually 0.25 to 0.5.
+ * @param deg default = 1. Degree of polynomial for linear regression model.
+ * @param seed Random seed for LOESS sampling.
+ * @param maxN default = 1000. Maximum number of data-points for LOESS method.
+ *  If this quantity exceeded random sampling is applied to data.
+ * @param colorBy default = "color" ("fill", "color", "paint_a", "paint_b", "paint_c").
+ *  Defines the color aesthetic for the geometry.
+ * @param fillBy default = "fill" ("fill", "color", "paint_a", "paint_b", "paint_c").
+ *  Defines the fill aesthetic for the geometry.
+ * @param mapping Set of aesthetic mappings.
+ *  Aesthetic mappings describe the way that variables in the data are
+ *  mapped to plot "aesthetics".
+ */
 class statSmooth(
     data: Map<*, *>? = null,
     geom: GeomOptions = GeomOptions(GeomKind.SMOOTH),
