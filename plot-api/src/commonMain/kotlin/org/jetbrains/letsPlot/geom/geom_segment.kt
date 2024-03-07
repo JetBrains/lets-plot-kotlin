d8e66c27ed08b761ee11cfcfc061592ec8bfa78b
Olga Larionova <olarionova@horis.ru>
2024-03-07 13:09:03 +0300

    Add links to examples.

--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/geom/geom_segment.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/geom/geom_segment.kt
@@ -27,6 +27,8 @@ import org.jetbrains.letsPlot.tooltips.TooltipOptions
  *
  * - [lines.ipynb](https://nbviewer.jupyter.org/github/JetBrains/lets-plot-kotlin/blob/master/docs/examples/jupyter-notebooks/lines.ipynb)
  *
+ * - [graph_edges.ipynb](https://nbviewer.jupyter.org/github/JetBrains/lets-plot-kotlin/blob/master/docs/examples/jupyter-notebooks/f-4.6.1/graph_edges.ipynb)
+ *
  * @param data The data to be displayed in this layer. If null, the default, the data
  *  is inherited from the plot data as specified in the call to [letsPlot][org.jetbrains.letsPlot.letsPlot].
  * @param stat The statistical transformation to use on the data for this layer.
