262d6c71dc89f17de6311575eda6c47289d508d8
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2024-11-01 15:22:18 -0400

    Set dev version

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ allprojects {
     group = "org.jetbrains.lets-plot"
     version = when (name) {
         "dokka" -> "4.9.0"
-        else -> "4.9.0"
+        else -> "4.9.1-SNAPSHOT"
 //        else -> "0.0.0-SNAPSHOT"  // for local publishing only
     }
 
