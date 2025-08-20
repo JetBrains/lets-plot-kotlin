1c5819c15694b76ca5387ace70346f958027f641
Artem Smirnov <asmirnov@horis.ru>
2025-08-20 13:06:41 +0200

    Dokka: Fix the docstrings associated with geomABLine().

--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/ABLineAesthetics.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/ABLineAesthetics.kt
@@ -12,13 +12,12 @@ import org.jetbrains.letsPlot.intern.OptionsCapsule
 /**
  * Aesthetic parameters supported by [geomABLine()][org.jetbrains.letsPlot.geom.geomABLine].
  *
- * @param slope The line slope.
- * @param intercept The value of y at the point where the line crosses the y-axis.
- * @param alpha Transparency level of a layer.
- *  Understands numbers between 0 and 1.
- * @param color Color of the geometry.
+ * @param slope Line slope.
+ * @param intercept Y-intercept, (value of y at x = 0).
+ * @param alpha Opacity; a number in [0, 1]. Lower values are more transparent (0 - transparent, 1 - opaque).
+ * @param color Line color.
  *  For more info see: [aesthetics.html#color-and-fill](https://lets-plot.org/kotlin/aesthetics.html#color-and-fill).
- * @param linetype Type of the line.
+ * @param linetype Line type.
  *  Accept codes or names (0 = "blank", 1 = "solid", 2 = "dashed", 3 = "dotted", 4 = "dotdash", 5 = "longdash", 6 = "twodash"),
  *  a hex string (up to 8 digits for dash-gap lengths),
  *  or a pattern `offset to listOf(dash, gap, ...)` / `listOf(dash, gap, ...)`.
