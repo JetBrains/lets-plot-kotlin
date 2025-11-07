fb935c1de4af1736d02c007fd797ccc1ad628b7d
Artem Smirnov <asmirnov@horis.ru>
2025-11-07 17:28:08 +0100

    Update KDocs for the WithGroupOption interface.

--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/WithGroupOption.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/WithGroupOption.kt
@@ -8,7 +8,27 @@ package org.jetbrains.letsPlot.intern.layer
 import org.jetbrains.letsPlot.intern.Options
 
 /**
- * @property group Grouping key. If not set, grouping may be inferred from other aesthetics (e.g., color, size).
+ * ## Data Grouping
+ *
+ * The `group` parameter is not a true aesthetic but controls how data is grouped for visualization:
+ *
+ * **Default Grouping Behavior:**
+ *
+ * Lets-Plot automatically groups data by discrete variables mapped to aesthetics like `color`, `shape`, `linetype`, etc.
+ * This creates separate visual elements (lines, paths, polygons) for each unique combination of these variables.
+ *
+ * **Explicit Group Control:**
+ *
+ * - Use `group = "variableName"` to group only by that specific variable, overriding default grouping
+ *
+ * - Use `group = listOf("var1", "var2", ...)` to group by the interaction of multiple variables
+ *
+ * - Use `group = emptyList()` to disable all the grouping completely
+ *
+ * @property group Data grouping control (not a true aesthetic).
+ *  Use a variable name to group by that variable,
+ *  a list of variables to group by their interaction,
+ *  or an empty list to disable all grouping.
  */
 interface WithGroupOption {
     val group: Any?
