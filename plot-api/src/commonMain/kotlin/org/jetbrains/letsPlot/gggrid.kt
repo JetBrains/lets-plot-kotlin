85ac5e57706726d2050e85870a7995665201d6d1
Artem Smirnov <asmirnov@horis.ru>
2025-11-11 13:36:12 +0100

    Small fixes in the KDocs.

--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/gggrid.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/gggrid.kt
@@ -52,8 +52,9 @@ import org.jetbrains.letsPlot.intern.filterNonNullValues
  *   Specifies how guides (legends and colorbars) should be treated in the layout.
  *   - "collect" - collect guides from all subplots, removing duplicates.
  *   - "keep" - keep guides in their original subplots; do not collect at this level.
- *   - "auto" - allow guides to be collected if an upper-level layout uses ``guides="collect"``; otherwise, keep them in subplots.
- * Duplicates are identified by comparing visual properties:
+ *   - "auto" - allow guides to be collected if an upper-level layout uses `guides = "collect"`; otherwise, keep them in subplots.
+ *
+ *   Duplicates are identified by comparing visual properties:
  *   - For legends: title, labels, and all aesthetic values (colors, shapes, sizes, etc.).
  *   - For colorbars: title, domain limits, breaks, and color gradient.
  *
