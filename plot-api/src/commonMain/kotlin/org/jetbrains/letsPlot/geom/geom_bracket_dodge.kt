864ce7b6c80b622e7488bca6d50ed676ab340c6a
Artem Smirnov <horis.asmirnov@gmail.com>
2026-03-13 18:12:09 +0100

    Add geomBracketDodge class.

---
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/geom/geom_bracket_dodge.kt
@@ -0,0 +1,84 @@
+package org.jetbrains.letsPlot.geom
+
+import org.jetbrains.letsPlot.Geom.bracketDodge
+import org.jetbrains.letsPlot.Stat
+import org.jetbrains.letsPlot.intern.Layer
+import org.jetbrains.letsPlot.intern.Options
+import org.jetbrains.letsPlot.intern.layer.*
+import org.jetbrains.letsPlot.intern.layer.geom.BracketDodgeAesthetics
+import org.jetbrains.letsPlot.intern.layer.geom.BracketDodgeMapping
+import org.jetbrains.letsPlot.intern.layer.geom.BracketDodgeParameters
+import org.jetbrains.letsPlot.intern.layer.geom.TextParameters
+import org.jetbrains.letsPlot.pos.positionIdentity
+import org.jetbrains.letsPlot.tooltips.TooltipOptions
+
+/**
+ * TODO
+ */
+class geomBracketDodge(
+    data: Map<*, *>? = null,
+    stat: StatOptions = Stat.identity,
+    position: PosOptions = positionIdentity,
+    showLegend: Boolean = false,
+    inheritAes: Boolean? = null,
+    manualKey: Any? = null,
+    sampling: SamplingOptions? = null,
+    tooltips: TooltipOptions? = null,
+    override val x: Any? = null,
+    override val y: Any? = null,
+    override val istart: Any? = null,
+    override val iend: Any? = null,
+    override val lenStart: Any? = null,
+    override val lenEnd: Any? = null,
+    override val label: Any? = null,
+    override val size: Any? = null,
+    override val linetype: Any? = null,
+    override val color: Any? = null,
+    override val alpha: Any? = null,
+    override val family: Any? = null,
+    override val fontface: Any? = null,
+    override val hjust: Any? = null,
+    override val vjust: Any? = null,
+    override val angle: Any? = null,
+    override val lineheight: Any? = null,
+    override val segmentColor: Any? = null,
+    override val segmentSize: Any? = null,
+    override val segmentAlpha: Any? = null,
+    override val labelFormat: String? = null,
+    override val naText: String? = null,
+    override val nudgeX: Number? = null,
+    override val nudgeY: Number? = null,
+    override val nudgeUnit: String? = null,
+    override val checkOverlap: Boolean? = null,
+    override val bracketShorten: Number? = null,
+    override val tipLengthUnit: String? = null,
+    override val dodgeWidth: Number? = null,
+    override val nGroup: Int? = null,
+    override val sizeUnit: String? = null,
+    override val colorBy: String? = null,
+    mapping: BracketDodgeMapping.() -> Unit = {}
+) : BracketDodgeAesthetics,
+    BracketDodgeParameters,
+    TextParameters,
+    WithSizeUnitOption,
+    WithColorOption,
+    Layer(
+        mapping = BracketDodgeMapping().apply(mapping).seal(),
+        data = data,
+        geom = bracketDodge(),
+        stat = stat,
+        position = position,
+        showLegend = showLegend,
+        inheritAes = inheritAes,
+        manualKey = manualKey,
+        sampling = sampling,
+        tooltips = tooltips
+    ) {
+    override fun seal(): Options {
+        return super<BracketDodgeAesthetics>.seal() +
+                super<BracketDodgeParameters>.seal() +
+                super<TextParameters>.seal() +
+                super<WithSizeUnitOption>.seal() +
+                super<WithColorOption>.seal()
+    }
+}
