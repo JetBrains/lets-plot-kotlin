4d70d84ce532b2a522ca33c9e8bee679ea584efb
Artem Smirnov <horis.asmirnov@gmail.com>
2026-03-16 19:26:34 +0100

    Fill KDocs for remaining classes and interfaces. Fix seal() method for the BracketDodgeParameters.

--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/BracketDodgeAesthetics.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/BracketDodgeAesthetics.kt
@@ -5,7 +5,44 @@ import org.jetbrains.letsPlot.intern.Options
 import org.jetbrains.letsPlot.intern.OptionsCapsule
 
 /**
- * TODO
+ * Aesthetic properties supported by [geomBracketDodge()][org.jetbrains.letsPlot.geom.geomBracketDodge].
+ *
+ * @param x Primary axis category for horizontal brackets.
+ * @param y Bracket level - the position along the y-axis at which the bracket is drawn for horizontal brackets.
+ * @param iStart Index of the dodged group at the bracket start. Accept integer values between 0 and `nGroup - 1`.
+ * @param iEnd Index of the dodged group at the bracket end. Accept integer values between 0 and `nGroup - 1`.
+ * @param lenStart Length of the tip at the bracket start (at `iStart`).
+ * @param lenEnd Length of the tip at the bracket end (at `iEnd`).
+ * @param label Text to add.
+ * @param size Font size.
+ * @param linetype Type of the bracket line.
+ *  Accept codes or names (0 = "blank", 1 = "solid", 2 = "dashed", 3 = "dotted", 4 = "dotdash", 5 = "longdash", 6 = "twodash"),
+ *  a hex string (up to 8 digits for dash-gap lengths),
+ *  or a pattern `offset to listOf(dash, gap, ...)` / `listOf(dash, gap, ...)`.
+ *  For more info see: [aesthetics.html#line-types](https://lets-plot.org/kotlin/aesthetics.html#line-types).
+ * @param color Color of the geometry.
+ *  For more info see: [aesthetics.html#color-and-fill](https://lets-plot.org/kotlin/aesthetics.html#color-and-fill).
+ * @param alpha Transparency level of a layer.
+ *  Accept values between 0 and 1.
+ * @param family Font family.
+ *  For more info see: [aesthetics.html#text](https://lets-plot.org/kotlin/aesthetics.html#text).
+ * @param fontface Font style and weight.
+ *  For more info see: [aesthetics.html#text](https://lets-plot.org/kotlin/aesthetics.html#text).
+ * @param hjust Horizontal text alignment relative to the x-coordinate.
+ *  Possible values: 0 or "left" - left-aligned (text starts at x), 0.5 or "middle" (default) - text is centered on x,
+ *  1 or "right" - right-aligned (text ends at x).
+ *  There are two special alignments: "inward" (aligns text towards the plot center) and "outward" (away from the plot center).
+ * @param vjust Vertical text alignment relative to the y-coordinate.
+ *  Accept either a numeric value or one of: "bottom", "center", or "top".
+ *  The numeric values 0, 0.5 (default), and 1 correspond to "bottom", "center", and "top", respectively.
+ *  There are two special alignments: "inward" (aligns text towards the plot center) and "outward" (away from the plot center).
+ * @param angle Text rotation angle in degrees.
+ * @param lineheight Line height multiplier applied to the font size in the case of multi-line text.
+ * @param segmentColor Color of the bracket line (the segments forming the bracket).
+ *  For more info see: [aesthetics.html#color-and-fill](https://lets-plot.org/kotlin/aesthetics.html#color-and-fill).
+ * @param segmentSize Width of the bracket line (the segments forming the bracket).
+ * @param segmentAlpha Transparency level of the bracket line.
+ *  Accept values between 0 and 1.
  */
 interface BracketDodgeAesthetics : OptionsCapsule {
     val x: Any?
--- plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/BracketParameters.kt
+++ plot-api/src/commonMain/kotlin/org/jetbrains/letsPlot/intern/layer/geom/BracketParameters.kt
@@ -5,7 +5,19 @@ import org.jetbrains.letsPlot.intern.Options
 import org.jetbrains.letsPlot.intern.OptionsCapsule
 
 /**
- * TODO
+ * Properties for parameters of [geomBracket()][org.jetbrains.letsPlot.geom.geomBracket].
+ *
+ * @param bracketShorten default = 0.
+ *  Symmetrically shorten the bracket by shifting both ends toward the center.
+ *  Expects values between 0 and 1, where 0 corresponds to no shortening and 1 to a fully collapsed bracket.
+ * @param tipLengthUnit default = "size" ("res", "identity", "size", "px").
+ *  Unit for the `lenStart` and `lenEnd` aesthetics.
+ *  Possible values:
+ *
+ *  - "res": the unit equals the smallest distance between data points along the corresponding axis;
+ *  - "identity": a unit of 1 corresponds to a difference of 1 in data space;
+ *  - "size": a unit of 1 corresponds to the diameter of a point with `size = 1`;
+ *  - "px": the unit is measured in screen pixels.
  */
 interface BracketParameters : OptionsCapsule {
     val bracketShorten: Number?
